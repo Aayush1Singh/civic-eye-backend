a36becc0154a8e43d671ac086eece1a34120a933
AAYUSH SINGH <aayush3708@outlook.com>
2025-06-22 10:48:19 +0530

    chaging access urls

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = [
      "http://127.0.0.1:5173",
      "http://localhost:8080",
     "https://query-interface-gleam.vercel.app",
-    "https://kanun-legalai.vercel.app/"
+    "https://kanun-legalai.vercel.app"
 ]
 
 app.add_middleware(
